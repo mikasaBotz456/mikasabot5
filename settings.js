400e25ca487ddc1b7e9cf6cfc2e6dc9894af1a33
mikasaBotz456 <108715025+mikasaBotz456@users.noreply.github.com>
2022-08-28 17:21:06 +0700

    update settings.js

--- settings.js
+++ settings.js
@@ -15,7 +15,7 @@ global.APIKeys = {
 // setting 
 global.autoread = false // auto read message
 global.autobio = true 
-global.userRegister = true 
+global.userRegister = false
 global.autoblok212 = true //ubah jadi true biar nmr 212 di blok sama bot
 
 //language
